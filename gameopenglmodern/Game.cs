40a1ddcfdec48dde8ca06e58b45d53f90a0ac385
pupungeggang2 <kyongkyong2892@gmail.com>
2026-04-06 01:08:21 +0900

    temp

--- gameopenglmodern/Game.cs
+++ gameopenglmodern/Game.cs
@@ -3,6 +3,8 @@ using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
 using OpenTK.Windowing.GraphicsLibraryFramework;
 using OpenTK.Mathematics;
+using static StbTrueTypeSharp.StbTrueType;
+using StbTrueTypeSharp;
 
 namespace DodgeGame
 {
@@ -11,10 +13,10 @@ namespace DodgeGame
         public bool GameOver = false;
         public float Delta;
         public Shader ShaderGame;
-        public int Texture, BufferTexture;
+        public int TextTexture, TextBuffer;
         public int VertexArrayObject, VertexBuffer, LUColor, LUMatrix, LUMode, LAPosition, LATexCoord;
         public Matrix4 CameraMatrix;
-        public FontObject Font;
+        public stbtt_bakedchar[] BakedChars;
 
         public Player PlayerGame;
         public List<Bullet> BulletList;
@@ -46,7 +48,7 @@ namespace DodgeGame
             ShaderGame = new Shader("vertex.vert", "fragment.frag");
             VertexArrayObject = GL.GenVertexArray();
             VertexBuffer = GL.GenBuffer();
-            BufferTexture = GL.GenBuffer();
+            TextBuffer = GL.GenBuffer();
             GL.BindVertexArray(VertexArrayObject);
             GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBuffer);
             float[] vertices = {
@@ -59,7 +61,32 @@ namespace DodgeGame
             LAPosition = GL.GetAttribLocation(ShaderGame.Handle, "aPosition");
             LATexCoord = GL.GetAttribLocation(ShaderGame.Handle, "aTexCoord");
 
-            Font = new FontObject();
+            byte[] ttfData = File.ReadAllBytes("font.ttf");
+
+            int bitmapWidth = 512;
+            int bitmapHeight = 512;
+            byte[] bitmapPixels = new byte[bitmapWidth * bitmapHeight];
+
+            BakedChars = new stbtt_bakedchar[96];
+
+            bool result = StbTrueType.stbtt_BakeFontBitmap(
+                ttfData, 0,             // 폰트 데이터 및 오프셋
+                32.0f,                  // 픽셀 단위 폰트 높이
+                bitmapPixels,           // 출력 비트맵 버퍼
+                bitmapWidth, bitmapHeight, 
+                32, 96,                 // 구울 글자 범위 (ASCII 시작값, 개수)
+                BakedChars              // 글자 메타데이터 출력
+            );
+
+            TextTexture = GL.GenTexture();
+            GL.BindTexture(TextureTarget.Texture2D, TextTexture);
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Red, 
+                        bitmapWidth, bitmapHeight, 0, 
+                        PixelFormat.Red, PixelType.UnsignedByte, bitmapPixels);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)TextureWrapMode.ClampToEdge);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)TextureWrapMode.ClampToEdge);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Nearest);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Nearest);
 
             CameraMatrix = Matrix4.CreateOrthographicOffCenter(-4.0f, 4.0f, -3.0f, 3.0f, -1.0f, 1.0f);
             GL.Viewport(0, 0, FramebufferSize.X, FramebufferSize.Y);
@@ -137,9 +164,9 @@ namespace DodgeGame
             GL.Enable(EnableCap.Blend);
             GL.BlendFunc(BlendingFactor.SrcAlpha, BlendingFactor.OneMinusSrcAlpha);
             GL.UseProgram(ShaderGame.Handle);
-            GL.Uniform1(LUMode, 0);
             GL.BindVertexArray(VertexArrayObject);
 
+            GL.Uniform1(LUMode, 0);
             GL.Disable(EnableCap.Texture2D);
             RenderHandler.RenderPlayer(this, PlayerGame);
             foreach (Bullet bullet in BulletList)
@@ -147,9 +174,37 @@ namespace DodgeGame
                 RenderHandler.RenderBullet(this, bullet);
             }
 
-            GL.Enable(EnableCap.Texture2D);
             GL.Uniform1(LUMode, 1);
-            RenderHandler.RenderUpperUI(this);
+            GL.Enable(EnableCap.Texture2D);
+
+            List<float> vertices = new List<float>();
+
+            float x = 20.0f; 
+            float y = 20.0f;
+            string text = "Hello World";
+
+            foreach (char c in text) {
+                stbtt_aligned_quad q = new stbtt_aligned_quad();
+                StbTrueType.stbtt_GetBakedQuad(BakedChars, 512, 512, c - 32, x, y, q, 1);
+                vertices.AddRange(new float[] {
+                    q.x0, q.y1, q.s0, q.t1,
+                    q.x0, q.y0, q.s0, q.t0,
+                    q.x1, q.y0, q.s1, q.t0,
+
+                    q.x0, q.y1, q.s0, q.t1,
+                    q.x1, q.y0, q.s1, q.t0,
+                    q.x1, q.y1, q.s1, q.t1
+                });
+            }
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, TextBuffer);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Count * sizeof(float), vertices.ToArray(), BufferUsageHint.DynamicDraw);
+            GL.BindTexture(TextureTarget.Texture2D, fontTextureId);
+            GL.VertexAttribPointer(LAPosition, 2, VertexAttribPointerType.Float, false, 2 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(LAPosition);
+            GL.VertexAttribPointer(LATexCoord, 2, VertexAttribPointerType.Float, false, 2 * sizeof(float), 2 * sizeof(float));
+            GL.EnableVertexAttribArray(LATexCoord);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, vertices.Count / 4);
             SwapBuffers();
         }
         
